e24bc3de5ba56dea1ac4cdce0bd9c5158232f67d
yuping <yuping@zhonglanmedia.com>
2019-09-17 22:36:40 +0800

    init

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ export function render(oldRender) {
   alert(appId)
   alert(!!code && !!appId)
   if (!!code && !!appId) {
+    alert(1)
     login({ code: code, appId: appId }).then(data=>{
+      alert(2)
       alert(data.data.token)
      data.data&&data.data.token&&localStorage.setItem('TOKEN', data.data.token);
     });
