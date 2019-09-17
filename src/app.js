90952e340b6b94ade2de8f58431eb5a92fea1cf3
yuping <yuping@zhonglanmedia.com>
2019-09-17 21:47:25 +0800

    init

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import { isMobile, UrlSearch } from '@/utils/utils';
 import { login } from '@/api/user';
 
 export function render(oldRender) {
-  localStorage.getItem('TOKEN')||(isMobile() && (window.location.href='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4ff80217b55375b5&redirect_uri=https%3A%2F%2Fzhihuizhan.net&response_type=code&scope=snsapi_userinfo&state=STATE&component_appid=wx45a4a88f12821319&connect_redirect=1#wechat_redirect'))
   const code = UrlSearch('code');
   const appId = UrlSearch('appid');
   if (code && appId) {
     login({ code: code, appId: appId }).then(data=>{
      data.data&&data.data.token&&localStorage.setItem('TOKEN', data.data.token);
     });
+  }else{
+    localStorage.getItem('TOKEN')|| (isMobile() && (window.location.href='https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx4ff80217b55375b5&redirect_uri=https%3A%2F%2Fzhihuizhan.net&response_type=code&scope=snsapi_userinfo&state=STATE&component_appid=wx45a4a88f12821319&connect_redirect=1#wechat_redirect'))
   }
   oldRender();
 }
